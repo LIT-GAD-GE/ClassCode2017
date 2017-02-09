4717631545c6d3af0a8748d181d11e60d58a8d76
lit-john <john.hannafin@lit.ie>
2017-02-09 13:01:48 +0000

    Added 4.3

---
+++ 4.3-SimpleDoorWithSwitch/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+	public bool switchEnabled = false;
+	public DoorController theDoor;
+	public bool doorOpen = false;
+	public bool doorMoving = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void OnSwitchTriggerEnter() {
+		switchEnabled = true;
+	}
+
+	public void OnSwitchTriggerExit() {
+		switchEnabled = false;
+	}
+
+	public void OnSpacebarPressed() {
+		if ((switchEnabled == true) && (doorOpen == false)) {
+			theDoor.open ();
+		} else if ((switchEnabled == true) && (doorOpen == true)) {
+			theDoor.close ();
+		}
+	}
+
+	public void OnDoorFullyOpen (){
+		doorOpen = true;
+		StartCoroutine ("doorCloseTimer");
+	}
+
+	public void OnDoorFullyClosed() {
+		doorOpen = false;
+
+	}
+
+	private IEnumerator doorCloseTimer() {
+		yield return new WaitForSeconds (0.5f);
+		theDoor.close ();
+
+	}
+
+}
+
---
+++ 4.3-SimpleDoorWithSwitch/Assets/Scripts/SwitchController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwitchController : MonoBehaviour {
+
+	public LevelManager theLevelManager;
+
+	void OnTriggerEnter2D(Collider2D other) {
+		theLevelManager.OnSwitchTriggerEnter ();
+	}
+
+	void OnTriggerExit2D(Collider2D other) {
+		theLevelManager.OnSwitchTriggerExit ();
+	}
+}
