ff2ac480398f3b0e8652ba8fa586ae31cf521f7a
lit-john <john.hannafin@lit.ie>
2017-02-28 21:48:49 +0000

    Added 999-SwitchCharacter

---
+++ 999-SwitchCharacter/Assets/Scripts/MultiHeroController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MultiHeroController : MonoBehaviour {
+	public GameObject HeroA;
+	public GameObject HeroB;
+
+
+
+	// Use this for initialization
+	void Awake () {
+		HeroB.SetActive (false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+
+	public void switchHeros(string heroName) {
+
+		if (heroName == HeroB.name) {
+			HeroA.SetActive(false);
+			HeroB.SetActive(true);
+		}
+	
+		if (heroName == HeroA.name) {
+			HeroB.SetActive(false);
+			HeroA.SetActive(true);
+		}
+
+	}
+}
---
+++ 999-SwitchCharacter/Assets/Scripts/SwitcherController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwitcherController : MonoBehaviour {
+	public MultiHeroController theMultiHeroController;
+
+	public GameObject currentlyActiveHero;
+
+
+	void OnTriggerEnter2D(Collider2D other) {
+		if (other.name == "HeroA") {
+			Debug.Log ("Entered Switcher - switching to B");
+			theMultiHeroController.switchHeros ("HeroB");
+		}
+		else if (other.name == "HeroB")
+		{
+				Debug.Log ("Entered Switcher - switching to a");
+				theMultiHeroController.switchHeros ("HeroA");
+		}
+	}
+
+}
