772c1a2a6000a9a6f06147bdccb3a0aa5972a4ce
lit-john <john.hannafin@lit.ie>
2017-03-01 07:27:35 +0000

    Added 999-Inventory
    This project is incomplete

---
+++ 999-Inventory/Assets/Scripts/CollectableController.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CollectableController : MonoBehaviour {
+	public LevelManager theLevelManager;
+
+	void OnTriggerEnter2D(Collider2D other) {
+		Debug.Log ("Collectable trigger entered");
+		theLevelManager.CollectableTriggerEntered (this, other);
+	}
+}
---
+++ 999-Inventory/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void CollectableTriggerEntered(CollectableController cc, Collider2D other) {
+
+	}
+}
