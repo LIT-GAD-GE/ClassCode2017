22b373734a340544326d4504cc7658b02113a3b3
lit-john <john.hannafin@lit.ie>
2017-03-01 07:05:38 +0000

    Added 999-SwitchCharacter

--- 999-SwitchCharacter/Assets/Scripts/MultiHeroController.cs
+++ 999-SwitchCharacter/Assets/Scripts/MultiHeroController.cs
@@ -2,34 +2,30 @@
 using System.Collections;
 
 public class MultiHeroController : MonoBehaviour {
+
+	/*
+	 * References to the two child Heros. Make sure that only one of them
+	 * is initially active. You do this via the inspector.
+	 * 
+	 * Notice for demonstration purposes I have set the x and y scale of HeroB
+	 * to be 0.5 i.e. made it smaller than HeroA.
+	 */
 	public GameObject HeroA;
 	public GameObject HeroB;
 
 
+	public void switchHeros() {
 
-	// Use this for initialization
-	void Awake () {
-		HeroB.SetActive (false);
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
-
-
-
-	public void switchHeros(string heroName) {
-
-		if (heroName == HeroB.name) {
-			HeroA.SetActive(false);
-			HeroB.SetActive(true);
-		}
-	
-		if (heroName == HeroA.name) {
+		/*
+		 * if HeroA is active switch to HeroB and vias versa
+		 */
+		if (HeroA.activeSelf) {
+			HeroA.SetActive (false);
+			HeroB.SetActive (true);
+		} 
+		else {
 			HeroB.SetActive(false);
 			HeroA.SetActive(true);
 		}
-
 	}
 }
--- 999-SwitchCharacter/Assets/Scripts/SwitcherController.cs
+++ 999-SwitchCharacter/Assets/Scripts/SwitcherController.cs
@@ -2,21 +2,60 @@
 using System.Collections;
 
 public class SwitcherController : MonoBehaviour {
+	/*
+	 * This is a reference to the MultiHeroController which must be set in the inspector
+	 */
 	public MultiHeroController theMultiHeroController;
 
+	/*
+	 * This is a reference to the currentlyActiveHero which must be initially set
+	 * via the inspector. Thereafter it will be updated automatically when Heros
+	 * get switched.
+	 */
 	public GameObject currentlyActiveHero;
 
 
 	void OnTriggerEnter2D(Collider2D other) {
-		if (other.name == "HeroA") {
-			Debug.Log ("Entered Switcher - switching to B");
-			theMultiHeroController.switchHeros ("HeroB");
+		/*
+		 * I have to do it this way so as to ensure don't constantly switch from one
+		 * to the other when you initiate an initial switch/
+		 */
+
+		if ((other.name == "HeroA") && (currentlyActiveHero.name == "HeroA")) {
+			/* HeroA has just walked into the swwitcher trigger and it is HeroA that
+			 * I have registered as the currently active hero so lets switch heros
+			 * but DON'T register that our new active hero is HeroB, we'll do this
+			 * when HeroB enters the trigger due to the fact that its been switched 
+			 * in.
+			 */
+			theMultiHeroController.switchHeros ();
+		} 
+		else if ((other.name == "HeroA") && (currentlyActiveHero.name != "HeroA")) {
+			/* HeroA has entered the trigger YET I don't have HeroA registered as
+			 * the currently active hero. This is because I have just switched to
+			 * HeroA and didn't set it as the currently active hero. I was waiting
+			 * until now to do so.
+			 */
+			currentlyActiveHero = other.gameObject;
 		}
-		else if (other.name == "HeroB")
-		{
-				Debug.Log ("Entered Switcher - switching to a");
-				theMultiHeroController.switchHeros ("HeroA");
+		else if ((other.name == "HeroB")  && (currentlyActiveHero.name == "HeroB")){
+			/* HeroB has just walked into the swwitcher trigger and it is HeroB that
+			 * I have registered as the currently active hero so lets switch heros
+			 * but DON'T register that our new active hero is HeroA, we'll do this
+			 * when HeroA enters the trigger due to the fact that its been switched 
+			 * in.
+			 */
+			theMultiHeroController.switchHeros ();
+		} 
+		else if ((other.name == "HeroB") && (currentlyActiveHero.name != "HeroB")) {
+			/* HeroB has entered the trigger YET I don't have HeroA registered as
+			 * the currently active hero. This is because I have just switched to
+			 * HeroB and didn't set it as the currently active hero. I was waiting
+			 * until now to do so.
+			 */
+			currentlyActiveHero = other.gameObject;
 		}
+
 	}
 
 }
