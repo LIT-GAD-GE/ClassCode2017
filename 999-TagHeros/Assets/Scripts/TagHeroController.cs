6208a9a4ddfdc47bea0af205f745fa2708b0f694
lit-john <john.hannafin@lit.ie>
2017-03-14 15:16:46 +0000

    Added 999-TagHero

---
+++ 999-TagHeros/Assets/Scripts/TagHeroController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TagHeroController : MonoBehaviour {
+	private SpriteRenderer mySpriteRenderer;
+	private MovementController myMovementController;
+	private Rigidbody2D myRigidBody;
+
+	void Awake() {
+		mySpriteRenderer = GetComponent<SpriteRenderer> ();
+		myMovementController = GetComponent<MovementController> ();
+		myRigidBody = GetComponent<Rigidbody2D> ();
+	}
+
+
+	void OnCollisionEnter2D(Collision2D coll) {
+		if (coll.gameObject.tag == "Hero") {
+
+			if (mySpriteRenderer.enabled != true) {
+				// Ok I'm the Hero that isn't visible
+				mySpriteRenderer.enabled = true;
+				myMovementController.enabled = true;
+				myRigidBody.isKinematic = false;
+			} else {
+				// Ok I'm the Hero that is visible
+				mySpriteRenderer.enabled = false;
+				myMovementController.enabled = false;
+				myRigidBody.velocity = new Vector2 (0, 0);
+				myRigidBody.isKinematic = true;
+			}
+
+		}
+	}
+		
+}
