d5359caa75dfac6acf32cddad6a7e89741c1c8e1
lit-john <John.Hannafin@lit.ie>
2017-03-29 10:09:56 +0100

    Added Stars

---
+++ 11.0-WalkingOnPlatforms2/Assets/Scripts/StarController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StarController : MonoBehaviour {
+	public LevelManager theLevelManager;
+
+	void OnTriggerEnter2D(Collider2D other) {
+
+		if (other.tag == "Player") {
+			theLevelManager.starCollected ();
+
+			// I am assuming that once a star is collected it is always
+			// destroyed, i.e. there is no game rule effecting this, so
+			// I am going to destroy now
+			GameObject.Destroy (gameObject);
+		}
+	}
+}
